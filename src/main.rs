ea3f5caddf52b1980d7ccc86a7c0d902ae1af1b8
Ercy Moreira Neto <fireball.vb@gmail.com>
2019-05-05 00:18:41 -0300

    Menubar quit

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,22 @@ extern crate gtk;
 use gtk::prelude::*;
 
 
-use gtk::{Button, Window, WindowType};
+use gtk::{Button, Window, WindowType, WindowPosition,
+          Menu, MenuBar, MenuItem};
+
+// upgrade weak reference or return
+#[macro_export]
+macro_rules! upgrade_weak {
+    ($x:ident, $r:expr) => {{
+        match $x.upgrade() {
+            Some(o) => o,
+            None => return $r,
+        }
+    }};
+    ($x:ident) => {
+        upgrade_weak!($x, ())
+    };
+}
 
 fn main() {
     if gtk::init().is_err() {
@@ -12,11 +27,36 @@ fn main() {
     }
 
     let window = Window::new(WindowType::Toplevel);
-    window.set_title("First GTK+ Program");
-    window.set_default_size(350, 70);
+    window.set_title("Calculator");
+    window.set_position(WindowPosition::Center);
+
+    let v_box = gtk::Box::new(gtk::Orientation::Vertical, 10);
+
+    window.set_default_size(400, 400);
     let button = Button::new_with_label("Click me!");
-    window.add(&button);
+
+    let menu = Menu::new();
+    let menu_bar = MenuBar::new();
+    let quit = MenuItem::new_with_label("Quit");
+
+    menu.append(&quit);
+
+    let file = MenuItem::new_with_label("File");
+    file.set_submenu(Some(&menu));
+    menu_bar.append(&file);
+
+    v_box.pack_start(&menu_bar, false, false, 0);
+
+    window.add(&v_box);
     window.show_all();
+
+    let window_weak = window.downgrade();
+    quit.connect_activate(move |_| {
+        let window = upgrade_weak!(window_weak);
+        window.destroy();
+        gtk::main_quit();
+        Inhibit(false);
+    });
 
     window.connect_delete_event(|_, _| {
         gtk::main_quit();
